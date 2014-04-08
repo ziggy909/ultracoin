19a2a75e46fb232051e3f412ac6631e44232ad25
dx11 <dx11coin@outlook.com>
2014-04-08 22:13:57 +0800

    Update version.cpp

--- src/version.cpp
+++ src/version.cpp
@@ -36,8 +36,8 @@ const std::string CLIENT_NAME("Ultracoin");
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID "qq"
-#    define GIT_COMMIT_DATE "26-12"
+#    define GIT_COMMIT_ID "dev"
+#    define GIT_COMMIT_DATE "4-8"
 #endif
 
 #define BUILD_DESC_FROM_COMMIT(maj,min,rev,build,commit) \
